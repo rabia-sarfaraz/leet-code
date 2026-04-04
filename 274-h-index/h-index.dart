86d940901e9901859f963b00b527d454dc1eec27
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-04 22:15:53 +0500

    Time: 8 ms (45.45%) | Memory: 155.8 MB (9.09%) - LeetSync

---
+++ 274-h-index/h-index.dart
@@ -0,0 +1,16 @@
+class Solution {
+  int hIndex(List<int> citations) {
+    citations.sort();
+
+    int n = citations.length;
+
+    for (int i = 0; i < n; i++) {
+      int h = n - i;
+      if (citations[i] >= h) {
+        return h;
+      }
+    }
+
+    return 0;
+  }
+}
