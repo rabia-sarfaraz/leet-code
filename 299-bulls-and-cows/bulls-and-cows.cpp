072dcc6e1dfd996355c7b053033310c7e65641d7
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-04 22:34:45 +0500

    Time: 0 ms (100.00%) | Memory: 8.8 MB (73.31%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls = 0;
+        int cows = 0;
+
+        vector<int> count(10, 0);
+
+        for (int i = 0; i < secret.size(); i++) {
+            if (secret[i] == guess[i]) {
+                bulls++;
+            } else {
+                int s = secret[i] - '0';
+                int g = guess[i] - '0';
+
+                // if count was negative, it means matching previously seen opposite digit
+                if (count[s] < 0) cows++;
+                if (count[g] > 0) cows++;
+
+                count[s]++;
+                count[g]--;
+            }
+        }
+
+        return to_string(bulls) + "A" + to_string(cows) + "B";
+    }
+};
