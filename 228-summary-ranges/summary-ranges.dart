f7e9699afe7074c7ab3eb7640ba17b9ad184ce12
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-04 17:33:08 +0500

    Time: 1 ms (100.00%) | Memory: 149.6 MB (41.18%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.dart
@@ -0,0 +1,25 @@
+class Solution {
+  List<String> summaryRanges(List<int> nums) {
+    List<String> result = [];
+
+    if (nums.isEmpty) return result;
+
+    int start = nums[0];
+
+    for (int i = 1; i <= nums.length; i++) {
+      if (i == nums.length || nums[i] != nums[i - 1] + 1) {
+        if (start == nums[i - 1]) {
+          result.add("$start");
+        } else {
+          result.add("$start->${nums[i - 1]}");
+        }
+
+        if (i < nums.length) {
+          start = nums[i];
+        }
+      }
+    }
+
+    return result;
+  }
+}
