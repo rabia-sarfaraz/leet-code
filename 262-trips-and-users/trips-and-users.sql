c66758d5c957d51c1376dcd0bc6c08f4533e0e83
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-04 22:10:54 +0500

    Time: 191 ms (50.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 262-trips-and-users/trips-and-users.sql
@@ -0,0 +1,19 @@
+SELECT 
+    request_at AS "Day",
+    ROUND(
+        SUM(CASE 
+                WHEN status != 'completed' THEN 1 
+                ELSE 0 
+            END)::decimal 
+        / COUNT(*), 
+    2) AS "Cancellation Rate"
+FROM Trips t
+JOIN Users c 
+    ON t.client_id = c.users_id 
+    AND c.banned = 'No'
+JOIN Users d 
+    ON t.driver_id = d.users_id 
+    AND d.banned = 'No'
+WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY request_at
+ORDER BY request_at;
