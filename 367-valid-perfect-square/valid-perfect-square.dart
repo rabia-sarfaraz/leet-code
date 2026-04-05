6f66e68c53553bc641d02c343288c2848ac5512f
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-05 10:31:50 +0500

    Time: 0 ms (100.00%) | Memory: 148.1 MB (60.00%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.dart
@@ -0,0 +1,21 @@
+class Solution {
+  bool isPerfectSquare(int num) {
+    int left = 1;
+    int right = num;
+
+    while (left <= right) {
+      int mid = left + (right - left) ~/ 2;
+      int square = mid * mid;
+
+      if (square == num) {
+        return true;
+      } else if (square < num) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return false;
+  }
+}
