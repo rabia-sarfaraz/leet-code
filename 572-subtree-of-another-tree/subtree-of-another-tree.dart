0f4d3c66794f4f6483f035ea3c845959bdd8afa0
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-08 10:23:01 +0500

    Time: 8 ms (75.00%) | Memory: 148.5 MB (79.17%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.dart
@@ -0,0 +1,19 @@
+class Solution {
+  bool isSubtree(TreeNode? root, TreeNode? subRoot) {
+    if (root == null) return false;
+
+    if (isSame(root, subRoot)) return true;
+
+    return isSubtree(root.left, subRoot) ||
+           isSubtree(root.right, subRoot);
+  }
+
+  bool isSame(TreeNode? a, TreeNode? b) {
+    if (a == null && b == null) return true;
+    if (a == null || b == null) return false;
+
+    if (a.val != b.val) return false;
+
+    return isSame(a.left, b.left) && isSame(a.right, b.right);
+  }
+}
