03cdb90d6bdbe859fc49e6efeb2fd9a184f79367
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-08 11:02:14 +0500

    Time: 0 ms (100.00%) | Memory: 152.7 MB (33.33%) - LeetSync

---
+++ 617-merge-two-binary-trees/merge-two-binary-trees.dart
@@ -0,0 +1,13 @@
+class Solution {
+  TreeNode? mergeTrees(TreeNode? root1, TreeNode? root2) {
+    if (root1 == null) return root2;
+    if (root2 == null) return root1;
+
+    TreeNode merged = TreeNode(root1.val + root2.val);
+
+    merged.left = mergeTrees(root1.left, root2.left);
+    merged.right = mergeTrees(root1.right, root2.right);
+
+    return merged;
+  }
+}
