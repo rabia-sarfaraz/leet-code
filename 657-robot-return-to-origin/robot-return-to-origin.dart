e6efb7ba6fe693824b3d62d54abecec314e5cfe1
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-08 18:16:11 +0500

    Time: 3 ms (44.64%) | Memory: 152.5 MB (7.14%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.dart
@@ -0,0 +1,19 @@
+class Solution {
+  bool judgeCircle(String moves) {
+    int x = 0, y = 0;
+
+    for (int i = 0; i < moves.length; i++) {
+      if (moves[i] == 'U') {
+        y++;
+      } else if (moves[i] == 'D') {
+        y--;
+      } else if (moves[i] == 'L') {
+        x--;
+      } else if (moves[i] == 'R') {
+        x++;
+      }
+    }
+
+    return x == 0 && y == 0;
+  }
+}
