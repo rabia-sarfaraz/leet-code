5db4f41b2e7e1b6e08ec9babbbac93daff58f796
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-08 18:11:57 +0500

    Time: 0 ms (100.00%) | Memory: 150.6 MB (100.00%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.dart
@@ -0,0 +1,14 @@
+class Solution {
+  int minSteps(int n) {
+    int res = 0;
+    int d = 2;
+    while (n > 1) {
+      while (n % d == 0) {
+        res += d;
+        n ~/= d;
+      }
+      d++;
+    }
+    return res;
+  }
+}
