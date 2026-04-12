988676720f714056a312ed25e8ba8b5c3adab268
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-12 09:49:05 +0500

    Time: 0 ms (100.00%) | Memory: 150.6 MB (12.50%) - LeetSync

---
+++ 961-long-pressed-name/long-pressed-name.dart
@@ -0,0 +1,18 @@
+class Solution {
+  bool isLongPressedName(String name, String typed) {
+    int i = 0, j = 0;
+
+    while (j < typed.length) {
+      if (i < name.length && name[i] == typed[j]) {
+        i++;
+        j++;
+      } else if (j > 0 && typed[j] == typed[j - 1]) {
+        j++; // long press repeat
+      } else {
+        return false;
+      }
+    }
+
+    return i == name.length;
+  }
+}
