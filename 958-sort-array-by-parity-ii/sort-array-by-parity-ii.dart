a52c78830748c5e2f850c30cdb268b68ca13f987
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-12 09:46:10 +0500

    Time: 0 ms (100.00%) | Memory: 152 MB (80.00%) - LeetSync

---
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.dart
@@ -0,0 +1,25 @@
+class Solution {
+  List<int> sortArrayByParityII(List<int> nums) {
+    int n = nums.length;
+    int i = 0; // even index
+    int j = 1; // odd index
+
+    while (i < n && j < n) {
+      if (nums[i] % 2 == 0) {
+        i += 2;
+      } else if (nums[j] % 2 == 1) {
+        j += 2;
+      } else {
+        // swap
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
+        i += 2;
+        j += 2;
+      }
+    }
+
+    return nums;
+  }
+}
