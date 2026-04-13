87cef03a3ee5e9ac25abe7343745327450c6f99c
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-13 14:38:27 +0500

    Time: 2 ms (0.00%) | Memory: 157.3 MB (100.00%) - LeetSync

---
+++ 1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.dart
@@ -0,0 +1,26 @@
+class Solution {
+  bool canThreePartsEqualSum(List<int> arr) {
+    int total = 0;
+
+    for (int num in arr) {
+      total += num;
+    }
+
+    if (total % 3 != 0) return false;
+
+    int target = total ~/ 3;
+    int sum = 0;
+    int count = 0;
+
+    for (int num in arr) {
+      sum += num;
+
+      if (sum == target) {
+        count++;
+        sum = 0;
+      }
+    }
+
+    return count >= 3;
+  }
+}
