a7dacec250c8d9bd43d5b95f733a41efae0eef4c
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-16 10:27:29 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (48.47%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string result = "";
+        
+        for (char ch : address) {
+            if (ch == '.') {
+                result += "[.]";
+            } else {
+                result += ch;
+            }
+        }
+        
+        return result;
+    }
+};
