5ca0c87597babcf00fda5022095941fdd1a03b13
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-18 10:08:17 +0500

    Time: 0 ms (100.00%) | Memory: 148.8 MB (100.00%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.dart
@@ -0,0 +1,22 @@
+class Solution {
+  List<int> getNoZeroIntegers(int n) {
+
+    bool hasZero(int x) {
+      while (x > 0) {
+        if (x % 10 == 0) return true;
+        x ~/= 10;
+      }
+      return false;
+    }
+
+    for (int a = 1; a < n; a++) {
+      int b = n - a;
+
+      if (!hasZero(a) && !hasZero(b)) {
+        return [a, b];
+      }
+    }
+
+    return [];
+  }
+}
