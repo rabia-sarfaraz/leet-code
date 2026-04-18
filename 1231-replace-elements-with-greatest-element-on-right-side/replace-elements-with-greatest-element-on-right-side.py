9929950d8864568a1649baece90054d192fc4924
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-04-18 09:50:26 +0500

    Time: 55 ms (29.22%) | Memory: 13.9 MB (79.83%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def replaceElements(self, arr):
+        n = len(arr)
+        max_right = -1
+
+        for i in range(n - 1, -1, -1):
+            temp = arr[i]
+            arr[i] = max_right
+            max_right = max(max_right, temp)
+
+        return arr
