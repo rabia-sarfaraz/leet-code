a260b29f76bc2f44ef8c6a1f76d722812f6e1b5e
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-05-18 13:57:20 +0500

    Time: 31 ms (100.00%) | Memory: 229.3 MB (0.00%) - LeetSync

--- 1622-max-value-of-equation/max-value-of-equation.dart
+++ 1622-max-value-of-equation/max-value-of-equation.dart
@@ -2,21 +2,20 @@ import 'dart:collection';
 
 class Solution {
   int findMaxValueOfEquation(List<List<int>> points, int k) {
-    // deque stores [value = yi - xi, xi]
     ListQueue<List<int>> dq = ListQueue();
 
-    int ans = -1 << 30;
+    int ans = -1000000000;
 
     for (var p in points) {
       int x = p[0];
       int y = p[1];
 
-      // remove points where xj - xi > k
+      // remove invalid points
       while (dq.isNotEmpty && x - dq.first[1] > k) {
         dq.removeFirst();
       }
 
-      // calculate answer
+      // calculate maximum value
       if (dq.isNotEmpty) {
         ans = ans > x + y + dq.first[0]
             ? ans
@@ -25,7 +24,7 @@ class Solution {
 
       int value = y - x;
 
-      // maintain decreasing order
+      // maintain decreasing deque
       while (dq.isNotEmpty && dq.last[0] <= value) {
         dq.removeLast();
       }
