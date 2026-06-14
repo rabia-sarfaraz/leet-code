d7e94a3c1b5fb3ae20c7ea68c8ff4030cd740499
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-06-14 08:21:04 +0500

    Time: 4 ms (27.44%) | Memory: 12.3 MB (56.98%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def restoreString(self, s, indices):
+        """
+        :type s: str
+        :type indices: List[int]
+        :rtype: str
+        """
+        n = len(s)
+        ans = [''] * n
+
+        for i in range(n):
+            ans[indices[i]] = s[i]
+
+        return ''.join(ans)
