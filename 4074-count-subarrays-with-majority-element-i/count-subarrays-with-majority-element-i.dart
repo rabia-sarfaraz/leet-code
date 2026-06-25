21a61d32df3aaef7991ed88bee5806b271d554c6
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-06-25 09:54:26 +0500

    Time: 53 ms (100.00%) | Memory: 151.6 MB (100.00%) - LeetSync

---
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.dart
@@ -0,0 +1,21 @@
+class Solution {
+  int countMajoritySubarrays(List<int> nums, int target) {
+    int n = nums.length;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+      int targetCount = 0;
+
+      for (int j = i; j < n; j++) {
+        if (nums[j] == target) targetCount++;
+
+        int len = j - i + 1;
+        if (targetCount > len ~/ 2) {
+          ans++;
+        }
+      }
+    }
+
+    return ans;
+  }
+}
