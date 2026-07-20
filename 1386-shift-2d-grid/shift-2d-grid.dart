5e529a5c06c1629d1ba75bc4629ff35a4707dade
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-07-20 11:31:43 +0500

    Time: 1 ms (100.00%) | Memory: 149.2 MB (100.00%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.dart
+++ 1386-shift-2d-grid/shift-2d-grid.dart
@@ -2,34 +2,25 @@ class Solution {
   List<List<int>> shiftGrid(List<List<int>> grid, int k) {
     int m = grid.length;
     int n = grid[0].length;
+    int total = m * n;
 
-    List<int> flat = [];
+    k %= total;
 
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        flat.add(grid[i][j]);
-      }
-    }
-
-    int len = flat.length;
-    k = k % len;
-
-    List<int> rotated = [
-      ...flat.sublist(len - k),
-      ...flat.sublist(0, len - k)
-    ];
-
-    List<List<int>> result =
+    List<List<int>> ans =
         List.generate(m, (_) => List.filled(n, 0));
 
-    int idx = 0;
-
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < n; j++) {
-        result[i][j] = rotated[idx++];
+        int idx = i * n + j;
+        int newIdx = (idx + k) % total;
+
+        int r = newIdx ~/ n;
+        int c = newIdx % n;
+
+        ans[r][c] = grid[i][j];
       }
     }
 
-    return result;
+    return ans;
   }
 }
