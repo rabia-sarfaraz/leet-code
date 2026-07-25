985599fb1899855aac25a8b5ea66b3a0ef9ef27b
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-07-25 11:26:34 +0500

    ✅ Maximum Product of Two Digits | Easy | dart | Runtime: 0 ms (100.00%) | Memory: 150.9 MB | 2026-07-25

---
+++ easy/3536-maximum-product-of-two-digits/solution.dart
@@ -0,0 +1,16 @@
+class Solution {
+  int maxProduct(int n) {
+    int max1 = 0, max2 = 0; // top two digits
+    while (n > 0) {
+      final d = n % 10;
+      if (d >= max1) {
+        max2 = max1;
+        max1 = d;
+      } else if (d > max2) {
+        max2 = d;
+      }
+      n ~/= 10;
+    }
+    return max1 * max2;
+  }
+}
