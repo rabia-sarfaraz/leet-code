1097b677e35da16003c3a0b763d9e0f7d38a4067
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-08-05 16:42:56 +0500

    ✅ Minimum Deletions to Make Character Frequencies Unique | Medium | dart | Runtime: 7 ms (100.00%) | Memory: 152.9 MB | 2026-08-05

---
+++ medium/1647-minimum-deletions-to-make-character-frequencies-unique/solution.dart
@@ -0,0 +1,24 @@
+class Solution {
+  int minDeletions(String s) {
+    List<int> freq = List.filled(26, 0);
+
+    for (int i = 0; i < s.length; i++) {
+      freq[s.codeUnitAt(i) - 97]++;
+    }
+
+    Set<int> used = {};
+    int deletions = 0;
+
+    for (int f in freq) {
+      while (f > 0 && used.contains(f)) {
+        f--;
+        deletions++;
+      }
+      if (f > 0) {
+        used.add(f);
+      }
+    }
+
+    return deletions;
+  }
+}
