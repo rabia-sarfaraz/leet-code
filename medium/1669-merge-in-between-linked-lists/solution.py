5eaea193f8f2143cc94099fac3c58846726caf4c
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-08-07 10:00:30 +0500

    ✅ Merge In Between Linked Lists | Medium | Python | Runtime: 363 ms (41.10%) | Memory: 23.6 MB | 2026-08-07

---
+++ medium/1669-merge-in-between-linked-lists/solution.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def mergeInBetween(self, list1, a, b, list2):
+        """
+        :type list1: ListNode
+        :type a: int
+        :type b: int
+        :type list2: ListNode
+        :rtype: ListNode
+        """
+        prev = list1
+        for _ in range(a - 1):
+            prev = prev.next
+
+        curr = prev
+        for _ in range(b - a + 2):
+            curr = curr.next
+
+        prev.next = list2
+
+        tail = list2
+        while tail.next:
+            tail = tail.next
+
+        tail.next = curr
+
+        return list1
