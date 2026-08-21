e30abbcc8c336a3142d0ec3f3bfbc36e98ddbdf7
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-08-21 12:00:28 +0500

    ✅ Stone Game VII | Medium | C++ | Runtime: 77 ms (89.78%) | Memory: 88.7 MB | 2026-08-21

--- medium/1690-stone-game-vii/solution.cpp
+++ medium/1690-stone-game-vii/solution.cpp
@@ -1,6 +1,28 @@
-            }
-        }
+class Solution {
+public:
+    int stoneGameVII(vector<int>& stones) {
+        int n = stones.size();
 
-        return dp[0][n - 1];
-    }
+        vector<int> prefix(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + stones[i];
+        }
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+
+                int removeLeft = (prefix[j + 1] - prefix[i + 1]) - dp[i + 1][j];
+
+                int removeRight = (prefix[j] - prefix[i]) - dp[i][j - 1];
+
+                dp[i][j] = max(removeLeft, removeRight);
+            }
+        }
+
+        return dp[0][n - 1];
+    }
 };
