5bd2eb6d03b859e5c3b00ca84ac44de3f088af47
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-08-22 08:55:54 +0500

    ✅ Minimum Length of String After Deleting Similar Ends | Medium | dart | Runtime: 0 ms (100.00%) | Memory: 148.8 MB | 2026-08-22

---
+++ medium/1750-minimum-length-of-string-after-deleting-similar-ends/solution.dart
@@ -0,0 +1,20 @@
+class Solution {
+  int minimumLength(String s) {
+    int left = 0;
+    int right = s.length - 1;
+
+    while (left < right && s[left] == s[right]) {
+      String ch = s[left];
+
+      while (left <= right && s[left] == ch) {
+        left++;
+      }
+
+      while (left <= right && s[right] == ch) {
+        right--;
+      }
+    }
+
+    return right - left + 1;
+  }
+}
