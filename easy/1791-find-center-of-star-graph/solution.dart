f2d3578aac2e9de7c7c6ea5a55265ef534ef997f
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-08-23 10:37:09 +0500

    ✅ Find Center of Star Graph | Easy | dart | Runtime: 0 ms (100.00%) | Memory: 230.7 MB | 2026-08-23

---
+++ easy/1791-find-center-of-star-graph/solution.dart
@@ -0,0 +1,12 @@
+class Solution {
+  int findCenter(List<List<int>> edges) {
+    int a = edges[0][0];
+    int b = edges[0][1];
+
+    if (edges[1][0] == a || edges[1][1] == a) {
+      return a;
+    }
+
+    return b;
+  }
+}
