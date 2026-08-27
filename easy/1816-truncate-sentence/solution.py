306580ee0a25fac80365c0c1158cbcbdb51bc20d
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-08-27 15:57:15 +0500

    ✅ Truncate Sentence | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 12.5 MB | 2026-08-27

---
+++ easy/1816-truncate-sentence/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def truncateSentence(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        words = s.split()
+
+        return " ".join(words[:k])
