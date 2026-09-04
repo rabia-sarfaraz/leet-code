acff1e03fe37a8eddd50f104e48c922e2fb16a3d
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-09-04 09:14:15 +0500

    ✅ Smallest Stable Index I | Easy | dart | Runtime: 2 ms (100.00%) | Memory: 152.2 MB | 2026-09-04

---
+++ easy/3903-smallest-stable-index-i/solution.dart
@@ -0,0 +1,27 @@
+class Solution {
+  int firstStableIndex(List<int> nums, int k) {
+    int n = nums.length;
+
+    // suffixMin[i] = nums[i..n-1] ka minimum
+    List<int> suffixMin = List.filled(n, 0);
+
+    suffixMin[n - 1] = nums[n - 1];
+
+    for (int i = n - 2; i >= 0; i--) {
+      suffixMin[i] = min(nums[i], suffixMin[i + 1]);
+    }
+
+    // Prefix maximum maintain karo
+    int prefixMax = nums[0];
+
+    for (int i = 0; i < n; i++) {
+      prefixMax = max(prefixMax, nums[i]);
+
+      if (prefixMax - suffixMin[i] <= k) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+}
