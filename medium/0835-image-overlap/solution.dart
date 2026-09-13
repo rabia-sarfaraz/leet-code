abdba73a5994f4cee0cd8bd3629e48ec4e849d9c
Rabia Sarfaraz <sarfarazrabi021@gmail.com>
2026-09-13 09:33:25 +0500

    ✅ Image Overlap | Medium | dart | Runtime: 70 ms | Memory: 149.8 MB | 2026-09-13

---
+++ medium/0835-image-overlap/solution.dart
@@ -0,0 +1,35 @@
+class Solution {
+  int largestOverlap(List<List<int>> img1, List<List<int>> img2) {
+    int n = img1.length;
+    int answer = 0;
+
+    // Har possible shift try karo
+    for (int dr = -(n - 1); dr <= n - 1; dr++) {
+      for (int dc = -(n - 1); dc <= n - 1; dc++) {
+        int count = 0;
+
+        for (int r = 0; r < n; r++) {
+          for (int c = 0; c < n; c++) {
+            int r2 = r + dr;
+            int c2 = c + dc;
+
+            if (r2 >= 0 &&
+                r2 < n &&
+                c2 >= 0 &&
+                c2 < n &&
+                img1[r][c] == 1 &&
+                img2[r2][c2] == 1) {
+              count++;
+            }
+          }
+        }
+
+        if (count > answer) {
+          answer = count;
+        }
+      }
+    }
+
+    return answer;
+  }
+}
